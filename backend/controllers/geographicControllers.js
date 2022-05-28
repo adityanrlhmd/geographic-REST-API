192fe209c19e474ca4070ca933c28f34c1f82711
adityanrlhmd <adityanurulhamda@gmail.com>
2022-05-28 23:00:15 +0700

    InitialREST API geographic

--- backend/controllers/geographicControllers.js
+++ backend/controllers/geographicControllers.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Geographic = require('../models/geographicModel')
+
 // @desc Get geographic
 // @route Get /api/geographic
 // @access Private
-const getGeographic = asyncHandler(async(req, res) => {
-    res.status(200).json({message: 'Get geographic'})
+const getGeographic = asyncHandler(async(req, res) => { 
+    const geographic = await Geographic.find()
+
+    res.status(200).json(geographic)
 })
 
 
@@ -16,21 +20,45 @@ const setGeographic = asyncHandler(async(req, res) => {
         res.status(400)
         throw new Error('Please add a text field')
     } 
-    res.status(200).json({message: 'Set geographic'})
+
+    const geographic = await Geographic.create({
+        text: req.body.text,
+    })
+    res.status(200).json(geographic)
 })
 
 // @desc Update geographic
 // @route PUT /api/geographic/:id
 // @access Private
 const updateGeographic = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `Update geographic ${req.params.id}`})
+    const geographic = await Geographic.findById(req.params.id)
+
+    if(!geographic) {
+        res.status(400)
+        throw new Error('Geographic not found')
+    }
+
+    const updateGeographic = await Geographic.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updateGeographic)
 })
 
 // @desc Delete geographic
 // @route DELETE /api/geographic/:id
 // @access Private
 const deleteGeographic = asyncHandler(async(req, res) => {
-    res.status(200).json({message: `Delete geographic ${req.params.id}`})
+    const geographic = await Geographic.findById(req.params.id)
+
+    if(!geographic) {
+        res.status(400)
+        throw new Error('Geographic not found')
+    }
+
+    await geographic.remove()
+
+    res.status(200).json({ id: req.params.id})
 })
 
 
